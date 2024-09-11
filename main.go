9cc827687cd804236bff7aa8fff916f17e33abd3
DenzeySenpai <marksolante221@gmail.com>
2024-09-11 11:22:01 +0800

    [UPDATE 3.0 : Clean]

--- main.go
+++ main.go
@@ -27,18 +27,47 @@ func Clear() {
 	cmd.Run()
 }
 
-func __Game__() {
-	const up = 'w'
-	const down = 's'
-	const left = 'a'
-	const right = 'd'
+const up = 'w'
+const down = 's'
+const left = 'a'
+const right = 'd'
+const size = 16
 
+func Controller(c rune, pos []int, p bool) {
+	var min int = 1
+	var max int = size - 1
+	if p {
+		min = 0
+		max = size
+	}
+	switch c {
+	case left:
+		if pos[1] > min {
+			pos[1]--
+		}
+	case right:
+		if pos[1] < max {
+			pos[1]++
+		}
+	case up:
+		if pos[0] > min {
+			pos[0]--
+		}
+	case down:
+		if pos[0] < max {
+			pos[0]++
+		}
+	}
+}
+
+func __Game__() {
 	const player = "▓▓"
 	const g = "░░"
+	const cat = "{}"
 	var pos = []int{0, 0}
 	var coin = []int{0, 0}
 	var score int
-	var moves int = 30
+	var moves int = (size * 2) + 6
 	var coinMovement = [4]rune{'w', 'a', 's', 'd'}
 
 	var bonus = 9
@@ -56,78 +85,40 @@ func __Game__() {
 		fmt.Println("Score: ", score)
 		fmt.Println("Moves: ", moves)
 		fmt.Println(" ")
-		var floor = [10][10]string{
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
-			{g, g, g, g, g, g, g, g, g, g},
+		var floor [size][size]string
+		for s := 0; s < size; s++ {
+			for d := 0; d < size; d++ {
+				floor[s][d] = g
+			}
 		}
 		floor[pos[0]][pos[1]] = player
-		floor[coin[0]][coin[1]] = "M"
+		floor[coin[0]][coin[1]] = cat
 		var i int
-		for i = 0; i < 10; i++ {
+		for i = 0; i < size; i++ {
 			fmt.Println(floor[i])
 		}
-		reader := bufio.NewReader(os.Stdin)
 		if moves == 0 {
+			fmt.Println("Game over!")
 			break
 		} else {
 			moves--
 		}
+		reader := bufio.NewReader(os.Stdin)
 		char, _, err := reader.ReadRune()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		switch char {
-		case left:
-			if pos[1] > 0 {
-				pos[1]--
-			}
-		case right:
-			if pos[1] < 10 {
-				pos[1]++
-			}
-		case up:
-			if pos[0] > 0 {
-				pos[0]--
-			}
-		case down:
-			if pos[0] < 10 {
-				pos[0]++
-			}
-		}
+		Controller(char, pos, true)
 
 		rand := rand.Intn(4)
 		move := coinMovement[rand]
-		switch move {
-		case left:
-			if coin[1] > 1 {
-				coin[1]--
-			}
-		case right:
-			if coin[1] < 9 {
-				coin[1]++
-			}
-		case up:
-			if coin[0] > 1 {
-				coin[0]--
-			}
-		case down:
-			if coin[0] < 9 {
-				coin[0]++
-			}
-		}
 
 		if coin[0] == pos[0] && coin[1] == pos[1] {
 			score++
 			moves = moves + bonus
+		} else {
+			Controller(move, coin, false)
 		}
 
 		Clear()
